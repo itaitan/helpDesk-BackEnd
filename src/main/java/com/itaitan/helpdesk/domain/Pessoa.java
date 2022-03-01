c15e1951af988b2d6d2b83caefa57b758a4f9ff6
itaitã Araujo <itaitan2@gmail.com>
2022-03-01 16:47:20 -0300

    update da classe Tecnico

--- src/main/java/com/itaitan/helpdesk/domain/Pessoa.java
+++ src/main/java/com/itaitan/helpdesk/domain/Pessoa.java
@@ -16,6 +16,8 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import org.hibernate.validator.constraints.br.CPF;
+
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.itaitan.helpdesk.domain.enums.Perfil;
 
@@ -31,6 +33,7 @@ public abstract class Pessoa implements Serializable{
 	
 	protected String nome;
 	
+	@CPF
 	@Column(unique = true)//Aqui esta sendo informado que o CPF será unico no banco de dados.
 	protected String cpf;
 	
