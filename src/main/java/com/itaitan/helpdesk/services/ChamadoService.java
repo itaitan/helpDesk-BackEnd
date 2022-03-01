db2ea73bf6f32852eb575a9fb1e29460e061a1d6
itaitã Araujo <itaitan2@gmail.com>
2022-03-01 19:52:08 -0300

    EndPoit referente ao Id de chamados

---
+++ src/main/java/com/itaitan/helpdesk/services/ChamadoService.java
@@ -0,0 +1,22 @@
+package com.itaitan.helpdesk.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itaitan.helpdesk.domain.Chamado;
+import com.itaitan.helpdesk.repositories.ChamadoRepository;
+import com.itaitan.helpdesk.services.exceptions.ObjectnotFoundException;
+
+@Service
+public class ChamadoService {
+	@Autowired
+	private ChamadoRepository repository;
+	
+	
+	public Chamado findById(Integer id) {
+		Optional<Chamado> obj = repository.findById(id);
+		return obj.orElseThrow(()-> new ObjectnotFoundException("Objeto não encontrado! ID: " + id));
+	}
+}
